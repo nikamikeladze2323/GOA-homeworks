f378d911d207deb30741c85c8a2e7228fc6dd527
nikamikeladze2323 <salomiqeladze@yahoo.com>
2024-06-08 10:30:47 +0400

    fourth commit

---
+++ day 1/Untitled-2.py
@@ -0,0 +1,11 @@
+name = "nikolozi"    #
+surname = "miqeladze"   #
+age = 14   #
+height = 166    #
+
+
+((name) " + " int(surname) " + " int(age) " + " int(height))
+ 
+
+
+
---
+++ lesson 4/classwork 4.py
@@ -0,0 +1,8 @@
+name = "data"
+surname = "diasamidze"
+age = 100
+
+print(name)
+print(surname)
+print(age)
+

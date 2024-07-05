16f0ab095acc288012e4dcc0ca100bc5eaf47cad
nikamikeladze2323 <salomiqeladze@yahoo.com>
2024-07-05 23:30:32 +0400

    eighth commit

---
+++ level 8/classwork 8.py
@@ -0,0 +1,19 @@
+print(5 == 5) #True
+print(7 >= 7) #True
+print(7 != 7) #False becouse this is not equal
+print(10 <= 10) #True
+print(20 > 10) #True
+print(100 < 1000) #True
+print(300 != 500) #True
+print(700 < 1000) #True
+print(20 >= 19.9) #True
+
+
+
+soil_moisture = 80
+print(soil_moisture < 100) #True 
+
+
+
+
+
---
+++ level 8/classwork/homework/homework 8.py
@@ -0,0 +1,29 @@
+box = 150 # მომხმარებელის ყუთების რაოდენობა
+computer = 240 # მომხამარებელის კომპიუტერების რაოდენობა
+
+print(box >= computer) # ჩვენ აქ გვიწერია მეტია ანტოლია ეს False იმიტომ წერს რომ ყუთების რაოდენობა არ უდრის კომპიუტერების რაოდენობას და ასევე ყუთები არ არის კომპიუტერებზე ბევრი.
+print(box <= computer) # ეს ტოლი არ არის მაგრამ ნაკლებია ამიტომ წერს True.
+print(box != computer) # აქ იმიტომ არის True რომ 150 და 240 ერთმანეთს არ უდრის.
+print(box == computer) # აქ False იმიტომ არის რომ 150 და 240 ერთმანეთის ტოლები არ არის.
+print(box < computer) # ეს იმიტომ არის True რადგან 150 ნაკლებია 240-ზე.
+print(box > computer) # აქ კი პირიქით არის და მაგიტომ არის False. 
+
+print(5 > 1) # True
+print(5 < 1) # False
+print(25 != 20) # True
+print(50 <= 100) # True
+print(50 >= 100) # False
+print(300 < 10000) # True
+print(1200 > 300) # True         
+print(340 <= 340) # True
+print(2437 == 2437) # True
+
+Num1 = 200
+Num2 = 100
+
+print(Num1 > Num2)
+
+changed_Num1 = 300
+
+print(Num1 < Num2)
+

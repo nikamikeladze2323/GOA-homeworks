0959081e13e8230bb3129486c9dc772fe766a1f8
nikamikeladze2323 <salomiqeladze@yahoo.com>
2024-07-12 23:16:05 +0400

    commit

---
+++ level 3/lesson 3.py
@@ -0,0 +1,24 @@
+num1 = 50
+num2 = 100
+num3 = 1000
+num4 = 200
+num5 = 200.5
+
+# რიცხვები
+
+# ეს არის ჩვენი რიცხვები და სახელები რომლებსაც გამოვიყენებთ 10 ცვლადის შესაქმნელად
+
+print(num1 + num2)  #1 
+print(num4 - num2)  #2 
+print(num3 // num4)  #3 
+print(num1 * num2)  #4  
+print(num3 // num5)  #5
+print(num5 * num4)  #6
+print(num3 + num4 // num4)  #7
+print(num4 * num1 - num2)  #8
+print(num5 + num4 // num2)  #9
+print(num3 - num4)  #10
+
+
+
+

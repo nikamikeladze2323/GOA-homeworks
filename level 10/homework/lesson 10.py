f69476e5ebf0b74cf8a10ef99093dc9e53a87a57
nikamikeladze2323 <salomiqeladze@yahoo.com>
2024-07-17 18:48:29 +0400

    commit

---
+++ level 10/homework/lesson 10.py
@@ -0,0 +1,39 @@
+for i in range(11):
+ print(i)
+ print("Hello world")
+
+num = 10
+for i in range(10):
+ print(i + num)
+
+for i in range(100,1000,200):
+ print(i)
+
+for i in range(100,0,-1):
+ print(i)
+
+counter = 0
+while counter < 10:
+ counter = counter + 1
+ print(counter)
+ print("Hello world")
+
+num1 = 10
+counter1 = 0
+while counter1 < 10:
+ counter1 = counter1 + 1
+ print(counter1 + num1)
+
+counter2 = 0
+while counter2 < 1000:
+ counter2 = counter2 + 200
+ print(counter2)
+
+counter3 = 100
+while counter3 > 0:
+ counter3 = counter3 - 1
+ print(counter3)
+
+
+
+

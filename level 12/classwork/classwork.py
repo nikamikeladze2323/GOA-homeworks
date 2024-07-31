cb6ee95896a80397c9e84313236bb5b60c033b17
nikamikeladze2323 <salomiqeladze@yahoo.com>
2024-07-31 15:43:10 +0400

    commit

---
+++ level 12/classwork/classwork.py
@@ -0,0 +1,31 @@
+# 1
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult")
+
+else:
+    print("You get discount")
+
+# 2
+
+ticket = int(input("Enter your number: "))
+
+if ticket == 6:
+    print("You won the house")
+
+elif ticket == 30:
+    print("You won hawaii island ticket")
+
+else:
+    print("You won one dollor")
+
+# 3
+
+age1 = int("Enter your age: ")
+is_stundet = input("Are You student")
+
+if age1 >= 18 == is_stundet("yes"):
+    print("You are not an adult you get discount")
+else:
+    print("You are an adult you dont get discount")
---
+++ level 12/homework/homework.py
@@ -0,0 +1,76 @@
+# 1
+counter = 0
+while counter < 100: 
+ counter = counter + 1
+ if counter != 50:
+  if counter != 51: 
+   if counter != 52:
+    if counter != 53:  
+     if counter != 54: 
+      if counter != 55:  
+       if counter != 56:
+        if counter != 57: 
+         if counter != 58:
+          if counter != 59:
+           print(counter)
+# 2
+
+num = 0 
+while num < 50: 
+ num = num + 1
+ print(num)
+
+# 3
+
+num1 = int(input("Enter your number kent or lucia up to ten: "))
+
+
+if 1 != num1:
+ if 3 != num1:
+  if 5 != num1:
+   if 7 != num1:
+    if 9 != num1:
+                   print("your number is kent")
+if 2 == num1:
+ if 4 == num1:
+  if 6 == num1:
+   if 8 == num1:
+    if 10 == num1:
+                   print("your number is lucia")
+
+# 4
+
+grade = int(input("Enter your grade: "))
+
+if grade == 100:
+ print("Grade A greate job")
+
+elif grade == 80:
+ print("Grade B good job")
+
+elif grade == 70:
+ print("Grade C not bad at all")
+
+elif grade == 60:
+ print("Grade D you need to learn more")
+
+else:
+ print("You failed")
+
+age = int(input("Enter your age: "))
+
+if age < 13:
+ print("You are kid")
+
+elif age < 17:
+ print("You are teenager")
+
+else:
+ print("You are an adult")
+
+
+
+
+
+
+

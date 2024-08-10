565ecbf237d7f4192145a9a60382c3b0a8f9d5d5
nikamikeladze2323 <salomiqeladze@yahoo.com>
2024-08-10 15:03:31 +0400

    commit

---
+++ Level 14/classwork/classwork.py
@@ -0,0 +1,49 @@
+# 1.
+list = [1, 2, 3, 4, 5]
+print("Nikolozi"[0])
+print("giorgi"[0, 1])
+print("mate"[0, 1, 2])
+print("mari"[0, 1, 2, 3])
+print("salome"[0, 1, 2, 3, 4])
+
+       # 0  1  3  4  5
+list1 = {2, 4, 6, 8, 10}
+       # -5 -4 -3 -2 -1
+# უარყოფითი
+print([-1])
+print([-2])
+print([-3])
+print([-4])
+print([-5])
+
+# დადებითი
+print([0])
+print([1])
+print([2])
+print([3])
+print([4])
+
+# 2.
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+num1 = ([1] + 10)
+num2 = ([0] - 9)
+num3 = ([4] * 6)
+
+# 3.
+listn = ["good", "is", "programming"]
+listn1 = [2, 1, 0]
+listn2 = [-1, -2, -3]
+
+# 4.
+list2 = ["I am strong"]
+print[1, 2, 0, 3, 4, 5, 6, 7, 8]
+
+
+
+
+
+
+
+
+
+

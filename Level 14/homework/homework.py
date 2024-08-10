b6a39254efc8e07f86d929e4640e7f4dcaff655b
nikamikeladze2323 <salomiqeladze@yahoo.com>
2024-08-10 22:29:25 +0400

    commit

---
+++ Level 14/homework/homework.py
@@ -0,0 +1,68 @@
+# 1.
+    #   0  1  2  3  4  5  6  7  8  9  10  11  12  13  14  15
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    #  -15 -14 -13 -12 -11 -10 -9 -8 -7 -6 -5 -4 -3 -2 -1
+
+# დადებითი
+
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+print(list[5])
+print(list[6])
+print(list[7])
+print(list[8])
+print(list[9])
+print(list[10])
+print(list[11])
+print(list[12])
+print(list[13])
+print(list[14])
+
+# უარყოფითი
+
+print(list[-15])
+print(list[-14])
+print(list[-13])
+print(list[-12])
+print(list[-11])
+print(list[-10])
+print(list[-9])
+print(list[-8])
+print(list[-7])
+print(list[-6])
+print(list[-5])
+print(list[-4])
+print(list[-3])
+print(list[-2])
+print(list[-1])
+
+# 2.
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+list1[0:3]
+
+print(list1)
+
+# 3.
+list2 = ["niko", "goga", "tom", "giorgi", "cow"]
+
+name = "nika"
+name1 = "meow"
+name2 = "gowi"
+name3 = "gio"
+name4 = "pig"
+
+print(name[0])
+print(name1[1])
+print(name2[2])
+print(name3[3])
+print(name4[4])
+
+# 4.
+
+list3 = [1, 2, 3, 4, 5]
+slice(1)
+print([1])

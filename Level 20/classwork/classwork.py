1705047f2b46e456189ef22ffa892c8b2a6d6db7
nikamikeladze2323 <salomiqeladze@yahoo.com>
2024-08-30 17:44:04 +0400

    commit

---
+++ Level 20/classwork/classwork.py
@@ -0,0 +1,12 @@
+for i in range(1, 100, 5):
+    print(i)
+
+for i in range(1, 100, 1):
+    print(str(i) + "ლუწია" )
+
+for i in range(1, 100, 2):
+    print(str(i) + "კენტია" )
+
+for i in range(1, 100):
+    if i % 5 == 0:
+       print(i) 
---
+++ Level 20/homework/homework.py
@@ -0,0 +1,15 @@
+# 1.
+for i in range(0, 100, 5):
+    print(i)
+# 2.
+# 1. ლუწია
+for i in range(0, 100, 2):
+    print(str(i) + ". ლუწია")
+# 2. კენტია
+for i in range(1, 100, 2):
+    print(str(i) + ". კენტია")
+# 3.
+for i in range(100):
+    if i % 5 == 0:
+        print(str(i) + ". ხუთის ჯერადია")
+

35011dd543fb215c0507c87b29f36084763c31ec
nikamikeladze2323 <salomiqeladze@yahoo.com>
2024-09-15 22:30:37 +0400

    commit

---
+++ Level 24/classwork/classork.py
@@ -0,0 +1,3 @@
+MY_VERY_GOOD_STUDENTS = ["JEMALIKO", "NUGZARIKO", "JIMSHERA", "SVARCHIKA MAYVALA", "ZVIADA", "ეს ოროსანი ბიჭები მერამდენე ოქმი უნდა აიღონ უფ"]
+NUMBERS = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+ხუთის_ჯერადები = [5, 10, 15, 20, 25, 30]

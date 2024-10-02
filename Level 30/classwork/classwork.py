c6555690b3eb8213eea9117a1848375e8096c99f
nikamikeladze2323 <salomiqeladze@yahoo.com>
2024-10-02 22:57:57 +0400

    commit

---
+++ Level 30/classwork/classwork.py
@@ -0,0 +1,14 @@
+my_list = ["მასწავლებელი",]
+my_second_list = ["სხვა ადამიანი",]
+name = input("Enter your name: ")
+
+if name == "gabrieli":
+    my_list.append(name)
+    print(my_list)
+else:
+    my_second_list.append(name)
+    print(my_second_list)
+    
+
+
+
